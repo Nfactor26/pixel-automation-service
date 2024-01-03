c1b2ffc0d6c33fc898f3bfc87f017193e58d3154
Nish26 <Nfactor26@gmail.com>
2024-01-03 13:38:02 +0530

    Move services api project to new repository
    Moved required projects from pixel-automation project to new repository for better management.

---
+++ src/Pixel.Automation.Web.Portal/Pages/Tests/StatisticsPage.razor.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Pixel.Automation.Web.Portal.Pages.Tests;
+
+public partial class StatisticsPage : ComponentBase
+{      
+    [Parameter]
+    public string TestId { get; set; }     
+}
---
+++ src/Pixel.Persistence.Core/Request/TriggerRequests.cs
@@ -0,0 +1,10 @@
+﻿using Pixel.Persistence.Core.Models;
+
+namespace Pixel.Persistence.Core.Request
+{
+    public record AddTriggerRequest(string TemplateId, SessionTrigger Trigger);
+
+    public record RunTriggerRequest(string TemplateId, SessionTrigger Trigger);
+
+    public record UpdateTriggerRequest(string TemplateId, SessionTrigger Original, SessionTrigger Updated);
+}
